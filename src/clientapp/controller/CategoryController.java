7fe648944b2f363e46a1ac3adef45bb51c18a990
RUTH <>
2025-01-10 13:25:14 +0100

    Category client, controller, interface and entity created

--- src/clientapp/controller/CategoryController.java
+++ src/clientapp/controller/CategoryController.java
@@ -5,10 +5,49 @@
  */
 package clientapp.controller;
 
+import java.util.Optional;
+import java.util.logging.Logger;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
 /**
  *
  * @author 2dam
  */
 public class CategoryController {
     
+
+       private Stage stage;
+    
+       private Logger logger = Logger.getLogger(SignInController.class.getName());
+       
+       private TableColumn tbcolIcon;
+       private TableColumn tbcolName;
+       private TableColumn tbcolDescription;
+       private TableColumn tbcolCreationDate;
+       private TableColumn tbcolPegi;
+       
+       
+       public void initialize(Parent root) {
+        logger.info("Initializing InfoView stage.");
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("User info");
+        stage.setResizable(false);
+        
+         tbcolIcon.setCellValueFactory(new PropertyValueFactory<>("icon"));
+         tbcolName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tbcolDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        tbcolCreationDate.setCellValueFactory(new PropertyValueFactory<>("creation date"));
+        tbcolPegi.setCellValueFactory(new PropertyValueFactory<>("pegi"));
+        
+        stage.show();
+    }
+
 }
--- src/clientapp/interfaces/ICategory.java
+++ src/clientapp/interfaces/ICategory.java
@@ -5,10 +5,24 @@
  */
 package clientapp.interfaces;
 
+import clientapp.model.CategoryEntity;
+import java.util.List;
+import javax.ws.rs.ClientErrorException;
+
 /**
  *
  * @author 2dam
  */
 public interface ICategory {
     
+     public String countREST() throws ClientErrorException;
+     public void edit(Object requestEntity, String id) throws ClientErrorException;
+     public CategoryEntity find(CategoryEntity responseType, String id) throws ClientErrorException;
+     public CategoryEntity findRange(CategoryEntity responseType, String from, String to) throws ClientErrorException;
+     public List <CategoryEntity>  listCategoriesbyCreationDate(CategoryEntity responseType) throws ClientErrorException;
+     public void create(Object requestEntity) throws ClientErrorException;
+     public List <CategoryEntity>  listCategoriesbyPegi(CategoryEntity responseType) throws ClientErrorException;
+     public CategoryEntity findAll(CategoryEntity responseType) throws ClientErrorException;
+     public void remove(String id) throws ClientErrorException;
+     
 }
